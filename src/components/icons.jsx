57968efaffd789023d0af6ccd9f25a47935569ea
aparajitarai <aparajitarai17@gmail.com>
2025-12-11 18:33:41 +0530

    Add Ask Experts page with Sun icon and fix routing

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -105,6 +105,20 @@ export const X = (props) => (
   </IconBase>
 )
 
+export const Sun = (props) => (
+  <IconBase {...props}>
+    <circle cx="12" cy="12" r="4" />
+    <path d="M12 2v2" />
+    <path d="M12 20v2" />
+    <path d="m4.93 4.93 1.41 1.41" />
+    <path d="m17.66 17.66 1.41 1.41" />
+    <path d="M2 12h2" />
+    <path d="M20 12h2" />
+    <path d="m6.34 17.66-1.41 1.41" />
+    <path d="m19.07 4.93-1.41 1.41" />
+  </IconBase>
+)
+
 export default {
   Search,
   ChevronDown,
@@ -119,4 +133,5 @@ export default {
   Instagram,
   Menu,
   X,
+  Sun,
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/explore" element={<ExploreIdeas />} />
